defb3bca98b474173ad4c9c136adbacabe22f954
Ryan <rphillips0071@gmail.com>
2025-05-25 12:23:51 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
-  base: '/patio-menu/',
+  base: '/drinkMenu/',
   plugins: [react()],
 })
+
