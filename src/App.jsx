f1aa4ba8a5b38af60c155b11acdc00806a657450
Ryan <rphillips0071@gmail.com>
2025-06-07 17:20:10 -0400

    Add new non-alcoholic drinks: Tropical Sunrise, Coconut Lime Cooler, Lemon Basil Smash

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // src/App.jsx
 import drinks from './assets/drinks.json';
+import mocktails from './assets/nonAlcoholicDricnks.json'
 import DrinkCard from './DrinkCard';
 import './index.css';
 
@@ -9,7 +10,7 @@ function App() {
       <div className="wave-top" />
 
       <div className="drink-list">
-              <h1 className="menu-title">Phillips Patio <br /> Drink Menu</h1>
+        <h1 className="menu-title">Phillips Patio <br /> Drink Menu</h1>
         {drinks.map((drink, index) => (
           <div key={index} className="drink-item">
             <h2 className="drink-name">{drink.name}</h2>
@@ -21,9 +22,25 @@ function App() {
           </div>
         ))}
       </div>
+
+      <div className="drink-list">
+        <h1 className="menu-title">Collins Corner</h1>
+        {mocktails.map((drink, index) => (
+          <div key={index} className="drink-item">
+            <h2 className="drink-name">{drink.name}</h2>
+            <p className="drink-description">{drink.description}</p>
+            <p className="drink-ingredients">
+              <strong>Ingredients:</strong> {drink.ingredients.join(', ')}
+            </p>
+            {index !== mocktails.length - 1 && <hr className="divider" />}
+          </div>
+        ))}
+      </div>
+
       <div className="wave-bottom" />
     </div>
   );
 }
+
 
 export default App;
